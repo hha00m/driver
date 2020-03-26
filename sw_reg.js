e0fa1c3e7fbe18d72189293f306d8ce037d10b51
hha00m <hayder_ok@yahoo.com>
2020-03-26 22:30:28 +0300

    pwa fix

--- sw_reg.js
+++ sw_reg.js
@@ -3,30 +3,28 @@ window.addEventListener('load', () => {
 });
 let installPromptEvent;
 
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  event.preventDefault();
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
   // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
-});
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
 
-var btnInstall;
-btnInstall.addEventListener('click', () => {
-  // Update the install UI to remove the install button
-  document.querySelector('#install-button').disabled = true;
-  // Show the modal add to home screen dialog
-  installPromptEvent.prompt();
-  // Wait for the user to respond to the prompt
-  installPromptEvent.userChoice.then((choice) => {
-    if (choice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    // Clear the saved prompt since it can't be used again
-    installPromptEvent = null;
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
   });
 });
 
